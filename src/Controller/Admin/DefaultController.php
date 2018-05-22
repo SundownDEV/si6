7a8dd20002c108f9c1a0ae88a957d1dfae716e9a
root <anonhq789@protonmail.com>
2018-05-22 15:54:43 +0200

    Blog template

---
+++ src/Controller/Admin/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sundowndev
+ * Date: 22/05/18
+ * Time: 13:24
+ */
+
+namespace App\Controller\Admin;
+
+
+class DefaultController
+{
+
+}
--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -30,13 +30,10 @@ use Twig\TwigFunction;
 class AppExtension extends AbstractExtension
 {
     private $parser;
-    private $localeCodes;
-    private $locales;
 
-    public function __construct(Markdown $parser, $locales)
+    public function __construct(Markdown $parser)
     {
         $this->parser = $parser;
-        $this->localeCodes = explode('|', $locales);
     }
 
     /**
@@ -50,39 +47,10 @@ class AppExtension extends AbstractExtension
     }
 
     /**
-     * {@inheritdoc}
-     */
-    public function getFunctions(): array
-    {
-        return [
-            new TwigFunction('locales', [$this, 'getLocales']),
-        ];
-    }
-
-    /**
      * Transforms the given Markdown content into HTML content.
      */
     public function markdownToHtml(string $content): string
     {
         return $this->parser->toHtml($content);
-    }
-
-    /**
-     * Takes the list of codes of the locales (languages) enabled in the
-     * application and returns an array with the name of each locale written
-     * in its own language (e.g. English, Français, Español, etc.).
-     */
-    public function getLocales(): array
-    {
-        if (null !== $this->locales) {
-            return $this->locales;
-        }
-
-        $this->locales = [];
-        foreach ($this->localeCodes as $localeCode) {
-            $this->locales[] = ['code' => $localeCode, 'name' => Intl::getLocaleBundle()->getLocaleName($localeCode, $localeCode)];
-        }
-
-        return $this->locales;
     }
 }
